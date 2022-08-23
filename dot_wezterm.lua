6b8a9945cdf6d73796c20e448e0dcfdac126b1a6
Luke Hsiao <luke@hsiao.dev>
2022-08-23 10:53:44 -0700

    feat(wezterm): add hyperlinks for linear tickets
    Refs: https://wezfurlong.org/wezterm/hyperlinks.html

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -14,4 +14,55 @@ return {
         bottom = 0,
     },
     enable_tab_bar = false,
+
+    hyperlink_rules = {
+        -- Linkify things that look like URLs and the host has a TLD name.
+        -- Compiled-in default. Used if you don't specify any hyperlink_rules.
+        {
+            regex = '\\b\\w+://[\\w.-]+\\.[a-z]{2,15}\\S*\\b',
+            format = '$0',
+        },
+
+        -- linkify email addresses
+        -- Compiled-in default. Used if you don't specify any hyperlink_rules.
+        {
+            regex = [[\b\w+@[\w-]+(\.[\w-]+)+\b]],
+            format = 'mailto:$0',
+        },
+
+        -- file:// URI
+        -- Compiled-in default. Used if you don't specify any hyperlink_rules.
+        {
+            regex = [[\bfile://\S*\b]],
+            format = '$0',
+        },
+
+        -- Linkify things that look like URLs with numeric addresses as hosts.
+        -- E.g. http://127.0.0.1:8000 for a local development server,
+        -- or http://192.168.1.1 for the web interface of many routers.
+        {
+            regex = [[\b\w+://(?:[\d]{1,3}\.){3}[\d]{1,3}\S*\b]],
+            format = '$0',
+        },
+
+        -- Make task numbers clickable
+        -- The first matched regex group is captured in $1.
+        {
+            regex = [[\bHSI-(\d+)\b]],
+            format = 'https://linear.app/hsiao/issue/$0',
+        },
+        {
+            regex = [[\bNS-(\d+)\b]],
+            format = 'https://linear.app/numbersstation/issue/$0',
+        },
+
+        -- Make username/project paths clickable. This implies paths like the following are for GitHub.
+        -- ( "nvim-treesitter/nvim-treesitter" | wbthomason/packer.nvim | wez/wezterm | "wez/wezterm.git" )
+        -- As long as a full URL hyperlink regex exists above this it should not match a full URL to
+        -- GitHub or GitLab / BitBucket (i.e. https://gitlab.com/user/project.git is still a whole clickable URL)
+        {
+            regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
+            format = 'https://www.github.com/$1/$3',
+        },
+    },
 }
