abd21063f4365343dc9c98fcf6148acd74aff53d
Luke Hsiao <luke@hsiao.dev>
2024-02-21 23:01:14 -0700

    chore(bashrc): commit to fish

---
+++ private_dot_config/private_fish/functions/fish_user_key_bindings.fish
@@ -0,0 +1,10 @@
+function fish_user_key_bindings
+    fzf_key_bindings
+
+    # Let Atuin control Ctrl+R
+    set -gx ATUIN_NOBIND "true"
+    atuin init fish | source
+    # bind to ctrl-r in normal and insert mode, add any other bindings you want here too
+    bind \cr _atuin_search
+    bind -M insert \cr _atuin_search
+end
