6db923b6956d75f7c4e6270e66ca5666ecf3ea62
Luke Hsiao <luke@hsiao.dev>
2026-03-03 21:40:05 -0700

    chore(fish): guard env var on file existence
    Signed-off-by: Luke Hsiao <luke@hsiao.dev>

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -4,11 +4,13 @@ set -gx COLORTERM 24bit
 set -gx EDITOR hx
 set -gx FZF_DEFAULT_COMMAND 'fd --type f --hidden --follow --exclude .git'
 set -gx FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
+if test -f ~/.github_token
+    set -gx GITHUB_TOKEN (cat ~/.github_token)
+end
 set -gx GRAB_HOME ~/Work
 set -gx PASSAGE_AGE rage
 set -gx ZSTD_CLEVEL 19
 set -gx ZSTD_NBTHREADS (math (nproc)/2)
-set -gx GITHUB_TOKEN (cat ~/.github_token)
 
 # clear fish greeting
 set -g fish_greeting
