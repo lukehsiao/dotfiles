ec49986397fb88c13a2afdd44753953f095a3a14
Luke Hsiao <luke@hsiao.dev>
2026-09-03 21:41:52 -0600

    chore: switch default agent to claude
    Signed-off-by: Luke Hsiao <luke@hsiao.dev>

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,7 +1,7 @@
 # Environment Variables
 # Default agent for the herdr (hdl, hds, hdlm, hsl) and tmux (tdl, tds, tdlm,
 # tsl) layout functions
-set -q AI_AGENT; or set -gx AI_AGENT omp
+set -q AI_AGENT; or set -gx AI_AGENT claude
 if test (uname) = Darwin
     set -gx BAT_THEME "Catppuccin Mocha"
 else
